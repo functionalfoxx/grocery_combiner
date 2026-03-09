d5f6694d96911bc9c3bd1cac080a45297859bc12
functionalfoxx <sabrinakirin@gmail.com>
2026-03-09 16:53:31 -0700

    Ingredients from all recipes now added to a list with collect_ingredients

--- ingredients.py
+++ ingredients.py
@@ -2,3 +2,12 @@ def add_recipe(all_recipes, recipe_data):
     if recipe_data is not None:
         all_recipes.append(recipe_data)
     return all_recipes
+
+def collect_ingredients(all_recipes):
+    all_ingredients = []
+
+    for recipe in all_recipes:
+        ingredients = recipe["ingredients"]
+        all_ingredients.extend(ingredients)
+
+    return all_ingredients
