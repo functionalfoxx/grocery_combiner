8372062cf5006ed6e0b310a8489882bc7faf36cf
functionalfoxx <sabrinakirin@gmail.com>
2026-03-09 16:46:47 -0700

    Added ingredients.py and add_recipe function to begin storing ingredients collected from recipe URLs

---
+++ ingredients.py
@@ -0,0 +1,4 @@
+def add_recipe(all_recipes, recipe_data):
+    if recipe_data is not None:
+        all_recipes.append(recipe_data)
+    return all_recipes
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from scraper import get_recipe
+from ingredients import add_recipe
+
+all_recipes = []
 
 url = input("Enter recipe URL: ").strip()
 
 recipe_data = get_recipe(url)
+all_recipes = add_recipe(all_recipes, recipe_data)
 
 if recipe_data is None:
     print("Could not extract recipe from this URL.")
