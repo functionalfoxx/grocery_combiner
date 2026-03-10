45af41d5624724f470cdbb7ff23394d2f3350ed1
functionalfoxx <sabrinakirin@gmail.com>
2026-03-09 17:19:31 -0700

    Updated main so that if there's a bad scrape, the recipe isn't stored and if there is <1 recipe, message prints that none were added

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ while True:
         break
 
     recipe_data = get_recipe(url)
+
+    if recipe_data is None:
+        print("Could not extract recipe from this URL.")
+        continue
+
     all_recipes = add_recipe(all_recipes, recipe_data)
 
-all_ingredients = collect_ingredients(all_recipes)
-display_ingredients(all_ingredients)
+if len(all_recipes) == 0:
+    print("No recipes were added.")
+else:
+    all_ingredients = collect_ingredients(all_recipes)
+    display_ingredients(all_ingredients)
 
 if recipe_data is None:
     print("\nCould not extract recipe from this URL.\n")
